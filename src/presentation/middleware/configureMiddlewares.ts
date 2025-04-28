80533ae0b46874bbe281f8bb86f3312371d07701
Michael Gonzalez <michaeljoelgd@gmail.com>
2025-04-28 11:18:33 -0500

    Arreglando la peticion patch

--- src/presentation/middleware/configureMiddlewares.ts
+++ src/presentation/middleware/configureMiddlewares.ts
@@ -15,7 +15,7 @@ export const configureMiddlewares = (app: express.Application) => {
   app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization'],
   }));
 
