93eb23f5341d7693551c09e6764dbc25c721cc90
mcardonar <mp.cardonar@javeriana.edu.co>
2025-04-28 18:22:12 -0500

    Cargando template INVESTIGADOR

--- src/application/services/pdf.service.ts
+++ src/application/services/pdf.service.ts
@@ -3,52 +3,52 @@ import puppeteer, { Browser } from 'puppeteer';
 import path from 'path';
 import fs from 'fs';
 
-
 export class PDFService {
-    public async generatePdf(data: { date: string }): Promise<Buffer> {
-        let browser: Browser | null = null;
-        
-        // Ruta absoluta a la plantilla EJS en src/templates
-        const templatePath = path.join(process.cwd(), 'src', 'templates','pdf', 'pdfTemplate.ejs');
-       
+  public async generatePdf(data: { date: string; type: 'consent' | 'evaluation' }): Promise<Buffer> {
+    let browser: Browser | null = null;
 
-        const logoHusiPath = path.resolve('src', 'templates', 'assets', 'logo-HUSI-ajustado-nuevo.png');
-        const logoPujPath = path.resolve('src', 'templates', 'assets', 'pontificia_universidad_javeriana_logo-320x130.jpg');
+    // Seleccionar la plantilla dependiendo del tipo
+    const templateFileName = data.type === 'consent' 
+      ? 'pdfConsentTemplate.ejs'
+      : 'pdfTemplate.ejs';
 
-        const logoHusiBase64 = fs.readFileSync(logoHusiPath).toString('base64');
-        const logoPujBase64 = fs.readFileSync(logoPujPath).toString('base64');
+    const templatePath = path.join(process.cwd(), 'src', 'templates', 'pdf', templateFileName);
 
-      
-        const logoHusiUri = `data:image/png;base64,${logoHusiBase64}`;
-        const logoPujUri = `data:image/jpeg;base64,${logoPujBase64}`;
+    const logoHusiPath = path.resolve('src', 'templates', 'assets', 'logo-HUSI-ajustado-nuevo.png');
+    const logoPujPath = path.resolve('src', 'templates', 'assets', 'pontificia_universidad_javeriana_logo-320x130.jpg');
 
-        const htmlContent = await ejs.renderFile(templatePath, {
-            ...data,
-            logoHusiUri,
-            logoPujUri,
-          });
-      
-         browser = await puppeteer.launch({
-            headless: true,
-            //sexecutablePath: '/usr/bin/chromium', // Asegúrate de que Chromium esté instalado en el contenedor
-            protocolTimeout: 60000,              // Aumenta el timeout a 60 segundos
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu'
-            ],
-        });
+    const logoHusiBase64 = fs.readFileSync(logoHusiPath).toString('base64');
+    const logoPujBase64 = fs.readFileSync(logoPujPath).toString('base64');
 
-        const page = await browser.newPage();
-        // Usamos 'domcontentloaded' para evitar esperar recursos innecesarios
-        await page.setContent(htmlContent, { waitUntil: 'load' });
-        const pdfArray = await Promise.race([
-            page.pdf({ format: 'A4', printBackground: true }),
-            new Promise((_, reject) => setTimeout(() => reject('Timeout al generar el PDF'), 60000)), 
-        ]);
-        await browser.close();
-        const pdfBuffer = Buffer.from(pdfArray as Uint8Array);
-        return pdfBuffer;
-    }
+    const logoHusiUri = `data:image/png;base64,${logoHusiBase64}`;
+    const logoPujUri = `data:image/jpeg;base64,${logoPujBase64}`;
+
+    const htmlContent = await ejs.renderFile(templatePath, {
+      ...data,
+      logoHusiUri,
+      logoPujUri,
+    });
+
+    browser = await puppeteer.launch({
+      headless: true,
+      protocolTimeout: 60000,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu'
+      ],
+    });
+
+    const page = await browser.newPage();
+    await page.setContent(htmlContent, { waitUntil: 'load' });
+
+    const pdfArray = await Promise.race([
+      page.pdf({ format: 'A4', printBackground: true }),
+      new Promise((_, reject) => setTimeout(() => reject('Timeout al generar el PDF'), 60000)),
+    ]);
+
+    await browser.close();
+    return Buffer.from(pdfArray as Uint8Array);
+  }
 }
