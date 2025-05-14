a597cbaf4dce6ff240352b9035c0102a7d5967a7
Kose117 <josemrodriguez@javeriana.edu.co>
2025-05-14 16:49:22 -0500

    ✨ Fix aggregation date field in StatsRepository to use 'updatedAt' instead of 'fecha_final'

--- src/infrastructure/database/repositories/stats.repository.impl.ts
+++ src/infrastructure/database/repositories/stats.repository.impl.ts
@@ -65,7 +65,7 @@ export class StatsRepositoryImpl implements IStatsRepository {
             },
             {
                 $group: {
-                    _id: { bucket: { $dateToString: { format: unit, date: '$fecha_final' } } },
+                    _id: { bucket: { $dateToString: { format: unit, date: '$updatedAt' } } },
                     evaluadas: { $sum: 1 }
                 }
             },
